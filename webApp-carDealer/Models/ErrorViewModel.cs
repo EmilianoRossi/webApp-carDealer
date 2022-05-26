f0d520cb22647bbb1ceebbec139740913f25fa16
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-05-26 10:36:25 +0200

    Add project files.

---
+++ webApp-carDealer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webApp_carDealer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
