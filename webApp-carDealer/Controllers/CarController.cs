edb5cade924342447ee29f4f211d251bbd13a7d4
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-05-26 14:46:11 +0200

    Controller car creato

---
+++ webApp-carDealer/Controllers/CarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webApp_carDealer.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
