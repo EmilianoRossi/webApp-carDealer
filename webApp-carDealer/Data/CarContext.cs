75babe90824b51037c95b444e36ea630c723de71
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-05-26 14:46:33 +0200

    database creato

---
+++ webApp-carDealer/Data/CarContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webApp_cardDealer.Models;
+
+namespace webApp_carDealer.Data
+{
+    public class CarContext : DbContext
+    {
+
+        DbSet<Car> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=webApp-carDealer;Integrated Security=True");
+        }
+
+
+    }
+}
